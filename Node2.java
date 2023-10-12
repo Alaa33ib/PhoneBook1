f5f9f793047efb93af381262cef8f953dfb449e1
layalalt <147754472+layalalt@users.noreply.github.com>
2023-10-12 22:00:54 +0300

    Update Node2.java

--- Node2.java
+++ Node2.java
@@ -1,26 +1,32 @@
-public class Node2 {
+public class Node2 //Nodes that contain Event as data
+{
 
 	private Event data;
 	private Node2 next;
 
-	public Node2(Event s){
+	public Node2(Event s)
+   {
 		data = s;
 		next = null;
-
 	}
    
-   public void setData(Event data){
-   this.data=data;}
-   public Event getData(){
-   return data;}
+   public void setData(Event data)
+   {
+    this.data = data;
+   }
+   
+   public Event getData()
+   {
+     return data;
+   }
 
-	public void setNext(Node2 n){
-		next = n;
+	public void setNext(Node2 next)
+   {
+		this.next = next;
 	}
-	public Node2 getNext(){
+   
+	public Node2 getNext()
+   {
 		return next;
 	}
-
-
-
 }
